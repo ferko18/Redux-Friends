1ea00dd063975d557dca28e81a4d96747e09eaba
Firealem Erko <firealemg@yahoo.com>
2019-05-16 18:25:28 -0400

    added sign up form

--- friends/src/views/SignUp.js
+++ friends/src/views/SignUp.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>
-      Sign Up is working 
-    </div>
+    <div className='signupForm'>
+    <form>
+        <input type='text' placeholder='name'></input>
+        <input type='email' placeholder='email'></input>
+        <input type='text' placeholder='username'></input>
+        <input type='password' placeholder='password'></input>
+        <button>Submit</button>
+    </form>
+  </div>
   )
 }
